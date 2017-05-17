01412c3a7e46f49fcd6aaf28949a3f943fc20c42
“valerievanzuijlen” <valerievanzuijlen@gmail.comssh-keygen -t rsa -C valerievanzuijlen@gmail.comgit config --global color.ui truegit config --global user.name valerievanzuijlengit config --global user.email valerievanzuijlen@gmail.com>
2017-05-17 14:46:22 +0200

    Print out bye-bye message

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -1,3 +1,6 @@
+# 0. Helper functions
+# ------------------------
+
 def print_divider
   puts "*" * 40
   puts "\n"
@@ -7,6 +10,9 @@ def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+
+# 1. Defining array `ingredients`
+# ------------------------
 
 NUM_POTATOES = 5
 NUM_EGGS = 6
@@ -19,6 +25,9 @@ ingredients = [
   { name: 'salt', quantity: 'some' },
 ]
 
+# 2. Printing out list of `ingredients`
+# ------------------------
+
 puts "*    Let's cook a Spanish Omelette!    *"
 print_divider
 
@@ -27,6 +36,9 @@ puts "You need the following ingredients:"
 ingredients.each do |ingredient|
   puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
 end
+
+# 3. Asking the user if he got all ingredients
+# ------------------------
 
 loop do
   puts "\n"
@@ -45,3 +57,108 @@ loop do
     puts "That's not a valid input. Try again."
   end
 end
+
+# 4. Defining array `steps`
+# ------------------------
+
+steps = [
+  { description: "Scrape potatoes", action: "scrape_potatoes" },
+  { description: "Cut potatoes into thick slices", action: "cut_potatoes" },
+  { description: "Chop the onion", action: "generic_recipe_step" },
+  { description: "Heat the oil in a large frying pan", action: "generic_recipe_step" },
+  { description: "Add potatoes to the pan", action: "add_potatoes_to_pan" },
+  { description: "Add onion to the pan", action: "generic_recipe_step" },
+  { description: "Strain potatoes and onions through a colander into a large bowl", action: "generic_recipe_step" },
+  { description: "Break the eggs", action: "break_eggs" },
+  { description: "Beat the eggs separately", action: "generic_recipe_step" },
+  { description: "Stir into the bowl the potatoes with plenty of salt", action: "generic_recipe_step" },
+  { description: "Heat a little of the strained oil in a smaller pan", action: "generic_recipe_step" },
+  { description: "Tip everything into the pan and cook on a moderate heat", action: "generic_recipe_step"},
+  { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
+  { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
+]
+
+# 5. Printing out list of `steps`
+# ------------------------
+
+print_divider
+puts "See below the different steps to follow:"
+
+steps.each_with_index do |step, index|
+  puts (index + 1).to_s + ") " + step[:description]
+end
+
+# 6. Defining `steps` actions
+# ------------------------
+
+def generic_recipe_step
+  puts "On it!"
+  print_progress_bar
+end
+
+def scrape_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Scraping potato #{counter}"
+    print_progress_bar
+  end
+end
+
+def cut_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Cutting potato #{counter} into thick slices"
+    print_progress_bar
+  end
+end
+
+def add_potatoes_to_pan
+  for counter in 1..NUM_POTATOES
+    print "Adding potato #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "Breaking egg #{counter}"
+    print_progress_bar
+  end
+end
+
+# 7. Defining `ask_if_ready` for step method
+# ------------------------
+
+def ask_if_ready(step, index)
+  puts "Are you ready for step #{index + 1}?\n(#{step[:description]})"
+  answer = gets.chomp
+
+  answer.upcase == 'Y'
+end
+
+# 8. Iterating to each of the steps
+# ------------------------
+
+steps.each_with_index do |step, index|
+  print_divider
+
+  loop do
+    ready = ask_if_ready(step, index)
+    break if ready
+
+    puts "OK, I will give you some extra time."
+    print_progress_bar
+  end
+
+  send(step[:action])
+end
+
+# 9. Exiting program
+# ------------------------
+
+print_divider
+puts "Enjoy!"
